d440a7542fe40a784fa264d0861a9a303b2e111f
shishi42 <benji.riviere72@hotmail.fr>
2024-05-13 07:19:33 +0200

    test

--- events/.postEmbed.js
+++ events/.postEmbed.js
@@ -30,6 +30,7 @@ module.exports = {
     if (tournament.dataValues.tournament_status == "Inscriptions en cours") embed.addFields({ name: '\u200B', value: `:small_blue_diamond: Fin des inscriptions le <t:${tournament.dataValues.tournament_date_close}:F>.` })
     if (tournament.dataValues.tournament_status == "Tournoi fini") {
       embed.addFields(
+        { name: '\u200B', value: '\u200B' },
         { name: ':trophy: Résultats', value: '\u200B' },
         { name: ':first_place: ', value: `<@${tournament.dataValues.tournament_first}>`, inline: true },
         { name: ':second_place:', value: `<@${tournament.dataValues.tournament_second}>`, inline: true },
