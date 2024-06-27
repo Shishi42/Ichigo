b507673bcc6406d7c026adb0b1ad283c4e4bdd5b
Shishi <37541779+Shishi42@users.noreply.github.com>
2024-06-27 14:59:09 +0200

    fix + ajout tags

--- commands/tags.js
+++ commands/tags.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js")
 
 module.exports = {
 
-  name: "list_tags",
+  name: "tags",
   description: "List the bot's registered tags",
   permission: null,
   dm: true,
