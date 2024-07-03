d1e0dec6cf002b3189c0cbf4c7a108155413b742
Shishi <37541779+Shishi42@users.noreply.github.com>
2024-07-04 01:47:01 +0200

    test

---
+++ commands/challonge_to_sheets.js
@@ -0,0 +1,23 @@
+const Discord = require("discord.js")
+
+module.exports = {
+
+  name: "challonge_to_sheets",
+  description: "Convert challonge team-listing to Google Sheets",
+  permission: Discord.PermissionFlagsBits.Administrator,
+  dm: true,
+  category: "Utility",
+  options: [
+    {
+      type: "string",
+      name: "teams",
+      description: "The teams",
+      required: true,
+      autocomplete: false,
+    }
+  ],
+
+  async run(bot, message, args) {
+    return await message.reply(args.get("teams").value.split('\n').map((team) => `${team.split(" (")[0]} ${team.split(" (")[1].slice(0,-1).split(" - ")[0]}\n${team.split(" (")[0]} ${team.split(" (")[1].slice(0,-1).split(" - ")[1]}\n${team.split(" (")[0]} ${team.split(" (")[1].slice(0,-1).split(" - ")[2]}\n`).join('\n'))
+  }
+}
