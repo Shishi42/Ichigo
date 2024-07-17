74f8e8bd62332e2a52b3c5f932ea66836f013120
shishi42 <benji.riviere72@hotmail.fr>
2024-07-17 14:00:07 +0200

    auto messages

--- ichigo.js
+++ ichigo.js
@@ -1,11 +1,9 @@
 const Discord = require("discord.js")
-const config = require("./config.json")
+const config = require("./dev_config.json")
 
 const bot = new Discord.Client({intents: 3276799})
-const jsonfile = require ("jsonfile")
 const fs = require("fs")
-
-const slashcommands_loader = require("./slashcommands_loader")
+const cron = require("cron")
 
 bot.commands = new Discord.Collection()
 bot.color = "BEF0ED"
@@ -15,5 +13,11 @@ fs.readdirSync("./events/").filter(f => f.endsWith(".js") && !f.startsWith('.'))
   let event = require(`./events/${file}`)
   bot.on(file.split(".js").join(""), event.bind(null, bot))
 })
+
+new cron.CronJob('00 00 07 * * wed', () => { bot.channels.fetch(config.general).then(channel => channel.send({ files: [{ attachment: './medias/poubelles.mp4' }], content: "Nous sommes mercredi, ça tombe bien c'est le jour où on sort les poubelles. 🗑️" }))}).start()
+
+new cron.CronJob('00 00 07 * * fri', () => { bot.channels.fetch(config.general).then(channel => channel.send({ files: [{ attachment: './medias/jus.png' }], content: "Bon vendredi jus la team :call_me:" }))}).start()
+
+new cron.CronJob('00 00 18 * * wed', () => { bot.channels.fetch(config.general).then(channel => channel.send({ files: [{ attachment: './medias/poubelles.mp4' }], content: "Nous sommes mercredi, ça tombe bien c'est le jour où on sort les poubelles. 🗑️" })) }).start()
 
 bot.login(config.token)
