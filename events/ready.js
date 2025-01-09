f44a41d9633e66536e7bb687b3949d5932797e13
shishi42 <benji.riviere72@hotmail.fr>
2025-01-09 14:41:42 +0100

    edit tags

--- events/ready.js
+++ events/ready.js
@@ -12,23 +12,12 @@ module.exports = async bot => {
     "twitter": "https://x.com/SunAfterTheBey",
     "youtube": "https://www.youtube.com/@SunAftertheReign",
 
-    "achats" : "Notre guide d'achat : https://discord.com/channels/1221611301332193371/1230832023459860490/1242931207260475462",
     "data" : "Notre base de données des produits Beyblade X : https://docs.google.com/spreadsheets/d/1Wshh88T0oDORXXr4F7xVYK0cjBRMXYJE5KzoPjss_no",
     "tournoi": "Sun After the Reign organise un tournoi tous les mois, le Beyblade Battle Tournament. Si une date n'est pas encore annoncée, alors ça arrivera bientôt dans https://discord.com/channels/1221611301332193371/1221670844279947316",
     "enregistrement": "N'oubliez pas de valider votre présence au tournoi dans https://discord.com/channels/1221611301332193371/1227020880614260866 sinon on va se tape <:chopan_pouce:1255261127055769700>",
-
-    "paris": "https://maps.app.goo.gl/3Zj4sCcDNsoYLc1e8",
-    "marseille": "https://maps.app.goo.gl/dbFiBetKeugX3gPe7",
-
-    "1on1" : "Le règlement **1 on 1** se joue seul et non en équipe, chaque joueur assemble et ne joue **qu'une seule et même toupie** lors de l'intégralité du tournoi, les matchs sont joués en 4 points avec la règle du gain de point en fonction du finish (xtreme, burst, over, spin).",
-    "3on3" : "Le règlement **3 on 3** se joue seul et non en équipe, chaque joueur assemble 3 toupies en n'utilisant **aucun composant en double**, lors d'un match le joueur joue un round avec chaque toupie dans l'ordre qu'il le souhaite, les matchs sont joués en 4 points avec la règle du gain de point en fonction du finish (xtreme, burst, over, spin).",
-    "3vs3" : "Le règlement **3 vs 3** se joue en équipe de 3 joueurs, chaque joueur assemble et ne joue **qu'une seule et même toupie** lors de l'intégralité du tournoi en n'utilisant **aucun composant en double** dans l'équipe, lors d'un match l'équipe joue 1 round avec chaque joueur dans l'ordre qu'elle le souhaite, les matchs sont joués en 3 points et chaque finish ne rapporte qu'un point.",
-    "simple_elim" : "Dans le format **simple élimination** il ne suffit de perdre qu'un seul match pour être éliminé du tournoi.",
-    "double_elim" : "Dans le format **double élimination** il faut perdre 2 matchs pour être éliminé du tournoi.",
+    "salon": "https://discordapp.com/channels/1221611301332193371/1221669702477021247/1297573105757196320",
 
     "streaming" : "https://www.streaming-espace.fr",
-
-    "salon" : "https://discordapp.com/channels/1221611301332193371/1221669702477021247/1297573105757196320"
   }
 
   bot.regles = {
