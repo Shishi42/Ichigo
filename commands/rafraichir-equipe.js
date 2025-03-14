b543740532d6ad48d1b8f8bce31c3d440d4a3ec5
shishi42 <benji.riviere72@hotmail.fr>
2025-03-14 22:44:38 +0100

    on test

--- commands/rafraichir-equipe.js
+++ commands/rafraichir-equipe.js
@@ -1,4 +1,5 @@
 const Discord = require("discord.js")
+const Canvas = require('@napi-rs/canvas')
 
 module.exports = {
 
@@ -16,7 +17,10 @@ module.exports = {
 
     for (team of teams) {
       await require(`../events/.postTeamEmbed.js`).run(bot, team, message.channel, true)  
-      message.guild.roles.fetch(team.dataValues.team_role).then(role => { role.setIcon(team.dataValues.team_logo) })
+      let canvas = Canvas.createCanvas(1000, 1000)
+      let context = canvas.getContext('2d')
+      context.drawImage(await Canvas.loadImage(team.dataValues.team_logo), 0, 0, canvas.width, canvas.height)
+      message.guild.roles.fetch(team.dataValues.team_role).then(role => { role.setIcon(canvas.encode('png')) })
     }
 
     return await message.editReply({ content: "Done.", ephemeral: true })
