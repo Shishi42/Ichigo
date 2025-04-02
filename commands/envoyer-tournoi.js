557f725cf5b8cee3c97dc284316f9bfabd5b4447
Shishi <37541779+Shishi42@users.noreply.github.com>
2025-04-02 13:23:54 +0200

    oh le con

--- commands/envoyer-tournoi.js
+++ commands/envoyer-tournoi.js
@@ -131,7 +131,7 @@ module.exports = {
         } else prog = new Date(parseInt(Math.floor(Date.now()+5)))
 
         new cron.CronJob(prog, async () => {
-          channel.type == Discord.ChannelType.GuildAnnouncement ? await message.followUp({ content: `-# <@&${args.get("ping").value}>\n## Un nouveau tournoi arrive à __${args.get("ville").value.toUpperCase()}__`, embeds: [embed], components: []}).then(message => message.crosspost()) : await message.followUp({ content: `-# <@&${args.get("ping").value}>\n## Un nouveau tournoi arrive à __${args.get("ville").value.toUpperCase()}__`, embeds: [embed], components: []})
+          message.channel.type == Discord.ChannelType.GuildAnnouncement ? await message.followUp({ content: `-# <@&${args.get("ping").value}>\n## Un nouveau tournoi arrive à __${args.get("ville").value.toUpperCase()}__`, embeds: [embed], components: []}).then(message => message.crosspost()) : await message.followUp({ content: `-# <@&${args.get("ping").value}>\n## Un nouveau tournoi arrive à __${args.get("ville").value.toUpperCase()}__`, embeds: [embed], components: []})
             
           return i.editReply({content: "C'est bon.", components: [], ephemeral: true})
         }).start()
