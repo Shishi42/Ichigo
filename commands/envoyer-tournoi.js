59f1e6518839373a705e23ede1d816b418cc8a7a
Shishi <37541779+Shishi42@users.noreply.github.com>
2025-04-03 16:56:59 +0200

    maj envoyer tournoi sans embed

--- commands/envoyer-tournoi.js
+++ commands/envoyer-tournoi.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js")
 const { fetch } = require('undici')
 const cron = require("cron")
+const { contentwarehouse_v1 } = require("googleapis")
 
 module.exports = {
 
@@ -12,20 +13,6 @@ module.exports = {
   options: [
     {
       type: "string",
-      name: "ville",
-      description: "Ville du tournoi",
-      required: true,
-      autocomplete: false,
-    },
-    {
-      type: "role",
-      name: "ping",
-      description: "Quel role ping",
-      required: true,
-      autocomplete: false,
-    },
-    {
-      type: "string",
       name: "nom",
       description: "Nom du tournoi",
       required: false,
@@ -35,6 +22,13 @@ module.exports = {
       type: "string",
       name: "description",
       description: "Description du tournoi",
+      required: false,
+      autocomplete: false,
+    },
+    {
+      type: "string",
+      name: "lieu",
+      description: "Lieu du tournoi",
       required: false,
       autocomplete: false,
     },
@@ -61,25 +55,32 @@ module.exports = {
     },
     {
       type: "string",
-      name: "lieu",
-      description: "Lieu du tournoi",
-      required: false,
-      autocomplete: false,
-    },
-    {
-      type: "string",
       name: "poster",
       description: "URL Poster URL du tournoi",
       required: false,
       autocomplete: false,
     },
     {
-        type: "string",
-        name: "lien",
-        description: "Lien du tournoi",
-        required: false,
-        autocomplete: false,
-      },
+      type: "string",
+      name: "lien",
+      description: "Lien du tournoi",
+      required: false,
+      autocomplete: false,
+    },
+    {
+      type: "string",
+      name: "infos",
+      description: "Infos supplémentaires du tournoi",
+      required: false,
+      autocomplete: false,
+    },
+    {
+      type: "role",
+      name: "ping",
+      description: "Quel role ping",
+      required: false,
+      autocomplete: false,
+    },
     {
       type: "string",
       name: "date_pub",
@@ -93,19 +94,30 @@ module.exports = {
 
     await message.deferReply({ephemeral: true})
 
-    let date = new Date(args.get("date").value.split('-')[0].split('/')[2], args.get("date").value.split('-')[0].split('/')[1] - 1, args.get("date").value.split('-')[0].split('/')[0], args.get("date").value.split('-')[1].split(':')[0], args.get("date").value.split('-')[1].split(':')[1], args.get("date").value.split('-')[1].split(':')[2])
+    let content = ""
+    let medias = []
+    let date = args.get("date") ? new Date(args.get("date").value.split('-')[0].split('/')[2], args.get("date").value.split('-')[0].split('/')[1] - 1, args.get("date").value.split('-')[0].split('/')[0], args.get("date").value.split('-')[1].split(':')[0], args.get("date").value.split('-')[1].split(':')[1], args.get("date").value.split('-')[1].split(':')[2]) : ""
 
-    let embed = new Discord.EmbedBuilder().setColor(bot.color)
-    
-    if(args.get("nom")) embed.setTitle(args.get("nom").value).setURL(bot.url)
-    
-    if (args.get("description")) embed.setDescription(args.get("description").value.replaceAll("\\n", "\n"))
-    if (args.get("poster")) embed.setImage(args.get("poster").value)
-    if (args.get("date")) embed.addFields({ name: ':small_orange_diamond: Date', value: `Le <t:${Math.floor(date)/1000}:F>`})
-    embed.addFields({ name: ':small_orange_diamond: Lieu', value: args.get("ville").value + (args.get("lieu") ? ` - ${args.get("lieu").value}` : "")})  
-    if (args.get("ruleset")) embed.addFields({ name: ':small_orange_diamond: Règlement', value: `${args.get("ruleset").value}`})
-    if (args.get("format")) embed.addFields({ name: ':small_orange_diamond: Format', value: `${args.get("format").value}`})
-    if (args.get("lien")) embed.addFields({ name: ":small_orange_diamond: Plus d'infos ⬇️", value: args.get("lien").value })          
+    if(args.get("ping")) content += `-# <@&${args.get("ping").value}>\n` + "\n"
+
+    if(args.get("nom")) content += `## ${args.get("nom").value}>` + "\n"
+
+    if(args.get("description")) content += `## ${args.get("description").value}>` + "\n"
+
+    if(args.get("description") || args.get("description")) content += "\n"
+
+
+    if(args.get("date") || args.get("lieu") || args.get("format") || args.get("ruleset") || args.get("infos")) {
+      content += ":small_orange_diamond: Informations :small_orange_diamond:" + "\n"
+      if (args.get("date")) content += `:date: Date : Le <t:${date}:D>, à partir de <t:${date}:t>` + "\n"
+      if (args.get("lieu")) content += `:map: Lieu : ${args.get("lieu").value}` + "\n"
+      if (args.get("format")) content += `:bar_chart: Format : ${args.get("format").value}` + "\n"
+      if (args.get("ruleset")) content += `:scroll: Règlement : ${args.get("ruleset").value}` + "\n"
+      if (args.get("infos")) content += `:loudspeaker: Informations supplémentaires : ${args.get("infos").value}` + "\n"
+      content += "\n"
+    }
+
+    if (args.get("poster")) medias.push({ attachment: args.get("poster").value })
 
     let row = new Discord.ActionRowBuilder()
       .addComponents(
@@ -119,7 +131,7 @@ module.exports = {
           .setStyle(Discord.ButtonStyle.Danger)
       )
     let collector = message.channel.createMessageComponentCollector({time: 30000, max: 1})
-    message.editReply({embeds: [embed], components: [row]})
+    message.editReply({content: content, files : medias, components: [row]})
 
     collector.on('collect', async i => {
       await i.deferUpdate()
@@ -131,7 +143,7 @@ module.exports = {
         } else prog = new Date(parseInt(Math.floor(Date.now()+5)))
 
         new cron.CronJob(prog, async () => {
-          message.channel.type == Discord.ChannelType.GuildAnnouncement ? await message.channel.send({ content: `-# <@&${args.get("ping").value}>\n## Un nouveau tournoi arrive à __${args.get("ville").value.toUpperCase()}__`, embeds: [embed], components: []}).then(message => message.crosspost()) : await message.channel.send({ content: `-# <@&${args.get("ping").value}>\n## Un nouveau tournoi arrive à __${args.get("ville").value.toUpperCase()}__`, embeds: [embed], components: []})
+          message.channel.type == Discord.ChannelType.GuildAnnouncement ? await message.channel.send({content : content, files : medias}).then(message => message.crosspost()) : await message.channel.send({content : content, files : medias})
             
           return i.editReply({content: "C'est bon.", components: [], ephemeral: true})
         }).start()
