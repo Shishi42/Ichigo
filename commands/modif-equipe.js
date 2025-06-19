62d89340d31e0c68b69ee88fbcbb837f2b46f7e6
Shishi <37541779+Shishi42@users.noreply.github.com>
2025-06-20 01:43:48 +0200

    quick fix

--- commands/modif-equipe.js
+++ commands/modif-equipe.js
@@ -120,7 +120,7 @@ module.exports = {
       let msg_logo = await require(`../events/.generateTeamImage.js`).run(bot, team, channel.id, "logo", args.get("logo").value)
       let msg_emoji = await require(`../events/.generateTeamImage.js`).run(bot, team, channel.id, "emoji", args.get("logo").value)
     
-      let server_emoji = await message.guild.emojis.create({ attachment: msg_emoji.attachments.first().url, name: team.dataValues.team_name.toLowerCase().normalize("NFD").replaceAll(' ', '_').replaceAll('-', '').replaceAll(/[\u0300-\u036f]/g, "") })
+      let server_emoji = await message.guild.emojis.create({ attachment: msg_emoji.attachments.first().url, name: team.dataValues.team_name.toLowerCase().normalize("NFD").replaceAll(' ', '_').replaceAll('-', '').replaceAll('(', '').replaceAll(')', '').replaceAll(/[\u0300-\u036f]/g, "") })
       await bot.Teams.update({ team_logo: channel.id + "/" + msg_logo.id, team_emoji: channel.id + "/" + msg_emoji.id + "/" + server_emoji.id }, { where: { team_id: id } })
       await message.guild.channels.fetch(team.dataValues.team_message.split('/')[0]).then(channel => channel.messages.fetch(team.dataValues.team_message.split('/')[1]).then(msg => msg.react(server_emoji)))
       // await message.guild.roles.fetch(team.dataValues.team_role).then(role => { role.setIcon(msg_emoji.attachments.first().url) })
