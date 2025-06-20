4db93865528e7fbc786c694de5f87b7e362ce3a7
Shishi <37541779+Shishi42@users.noreply.github.com>
2025-06-20 20:36:22 +0200

    Create say.js

---
+++ commands/say.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js")
+
+module.exports = {
+
+  name: "say",
+  description: "Send back what you say",
+  permission: Discord.PermissionFlagsBits.Administrator,
+  dm: false,
+  category: "Utilitaire",
+  options: [
+    {
+      type: "string",
+      name: "text",
+      description: "The text you want the bot to say back",
+      required: true,
+      autocomplete: false,
+    }
+  ],
+
+  async run(bot, message, args) {
+    await message.channel.send(args.get("text").value)
+    return await message.reply({content: "Done.", ephemeral: true})
+  }
+}
