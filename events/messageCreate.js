521f0e6af8712840fd55906712cb20faba0b5496
Shishi <37541779+Shishi42@users.noreply.github.com>
2025-07-18 21:50:49 +0200

    mieux

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -4,7 +4,7 @@ module.exports = async (bot, message) => {
 
   if (message.content.startsWith('!') && message.content.slice(1) in bot.tags) message.channel.send(bot.tags[message.content.slice(1)])
 
-  if ((message.content.includes("quoi") || message.content.includes("koi")) && message.channel.parentId == 1221670118182879232) {
+  if ((message.content.includes("quoi") || message.content.includes("koi")) && !message.content.includes("feur") && (message.channel.parentId == 1221670118182879232 || message.channel.parentId == 1275521362596986880)) {
     message.react("🇫")
     message.react("🇪")
     message.react("🇺")
