617eb1869d4e25c88495335f5032756b188e7008
Shishi <37541779+Shishi42@users.noreply.github.com>
2025-07-18 12:28:10 +0200

    feur update

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -3,4 +3,6 @@ module.exports = async (bot, message) => {
   if (message.author.bot || message.channel.type === "dm") return
 
   if (message.content.startsWith('!') && message.content.slice(1) in bot.tags) message.channel.send(bot.tags[message.content.slice(1)])
+
+  if ((message.content.includes("quoi") || message.content.includes("koi")) && message.channel.parentId == 1221670118182879232) message.react(":regional_indicator_f:").react(":regional_indicator_e:").react(":regional_indicator_u:").react(":regional_indicator_r:")
 }
