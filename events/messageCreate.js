b9d0acc0f97b61bb2c7354c77dcb0b653795f36e
Shishi <37541779+Shishi42@users.noreply.github.com>
2025-07-18 12:30:29 +0200

    fix

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -4,5 +4,10 @@ module.exports = async (bot, message) => {
 
   if (message.content.startsWith('!') && message.content.slice(1) in bot.tags) message.channel.send(bot.tags[message.content.slice(1)])
 
-  if ((message.content.includes("quoi") || message.content.includes("koi")) && message.channel.parentId == 1221670118182879232) message.react(":regional_indicator_f:").react(":regional_indicator_e:").react(":regional_indicator_u:").react(":regional_indicator_r:")
+  if ((message.content.includes("quoi") || message.content.includes("koi")) && message.channel.parentId == 1221670118182879232) {
+    message.react(":regional_indicator_f:")
+    message.react(":regional_indicator_e:")
+    message.react(":regional_indicator_u:")
+    message.react(":regional_indicator_r:")
+  }
 }
