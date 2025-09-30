e9a985ad8bcd345f25cadd86353061728d1ccd8c
Shishi <37541779+Shishi42@users.noreply.github.com>
2025-09-30 20:16:27 +0200

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -4,10 +4,10 @@ module.exports = async (bot, message) => {
 
   if (message.content.startsWith('!') && message.content.slice(1) in bot.tags) message.channel.send(bot.tags[message.content.slice(1)])
 
-  if ((message.content.toLowerCase().includes("quoi") || message.content.toLowerCase().includes("koi")) && !message.content.toLowerCase().includes("feur") && (message.channel.parentId == 1221670118182879232 || message.channel.parentId == 1275521362596986880)) {
+  /*if ((message.content.toLowerCase().includes("quoi") || message.content.toLowerCase().includes("koi")) && !message.content.toLowerCase().includes("feur") && (message.channel.parentId == 1221670118182879232 || message.channel.parentId == 1275521362596986880)) {
     message.react("🇫")
     message.react("🇪")
     message.react("🇺")
     message.react("🇷")
-  }
+  }*/
 }
